87c7bb937cf7556e9406007f2809ff40c66b9ba3
rizkijanuarr <72480809+rizkijanuarr@users.noreply.github.com>
2020-10-20 18:17:48 +0700

    Update PROGRAM MENU SEGITIGA.cpp

--- PROGRAM MENU SEGITIGA.cpp
+++ PROGRAM MENU SEGITIGA.cpp
@@ -3,13 +3,17 @@
 /* 20051397046 */
 /* 2020B */
 
-#include<iostream>
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-#include<math.h>
-#include <stdlib.h>
+
+
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
 #include <cstdlib>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
 
 
 using namespace std;
@@ -18,32 +22,32 @@ int main()
 {
 char ket [225], yt;
 int alas, kode, a,d,e;
-float f, g, c;
 
-Awal : 
+Inputan Awal : 
 system ("cls");
 system ("color 0A");
-	cout<<" Nama		: Rizki Januar Irmansyah "<<endl;
-	cout<<" Kelas/NIM	: Manajamen Informatika 2020B/20051397046 "<<endl;
-	cout<<" Dosen		: Andi Iwan Nur Hidayat  "<<endl;
-    cout<<" Matkul		: Tugas Pratikum 2       "<<endl;
-    cout<<"============================================================="<<endl;
-	cout<<"              MENU MENHITUNG SEGITIGA SIKU-SIKU               "<<endl;
-	cout<<"=============================================================\n";
-    cout<<endl;
+cout<<" Nama		: Rizki Januar Irmansyah "<<endl;
+cout<<" Kelas/NIM	: Manajamen Informatika 2020B/20051397046 "<<endl;
+cout<<" Dosen		: Andi Iwan Nur Hidayat  "<<endl;
+cout<<" Matkul		: Tugas Pratikum 2       "<<endl;
+cout<<"============================================================="<<endl;
+cout<<"              MENU MENHITUNG SEGITIGA SIKU-SIKU               "<<endl;
+cout<<"=============================================================\n";<<endl;
+cout<<endl;
     
-	cout << "1. HITUNG PANJANG SISI MIRING " << endl;
-	cout << "2. HITUNG LUAS " << endl;
-	cout << "3. HITUNG KELILING " << endl;
-	cout << "4. KELUAR PROGRAM " << endl;
-	cout << "" << endl;
+cout<< "1. HITUNG PANJANG SISI MIRING "<<endl;
+cout<< "2. HITUNG LUAS "<<endl;
+cout<< "3. HITUNG KELILING "<<endl;
+cout<< "4. KELUAR PROGRAM "<<endl;
+cout<<endl;
 
-	cout << "SILAHKAN PILIH [1/2/3/4] : ";
-
+cout<< "SILAHKAN PILIH [1/2/3/4] : ";
+cout<<endl;
 	
 kode=getche();
 switch (kode)
 {
+/* Inputan Kesatu */
 case '1' : {
 cout<<endl;	
 cout<<"\nHITUNG PANJANG SISI MIRING " << endl;
@@ -62,24 +66,25 @@ break;
 }
 
 
+/* Inputan Kedua */
 case '2' : {
 cout<<endl;
-cout << "\nHITUNG LUAS" << endl;
+cout<< "\nHITUNG LUAS" << endl;
 cout<<endl;
-cout << "MASUKAN ALAS NYA	: "; cin >> d;
-cout << "MASUKAN TINGGI NYA	: "; cin >> e;
+cout<< "MASUKAN ALAS NYA	: ";cin>>d;
+cout<< "MASUKAN TINGGI NYA	: ";cin>>e;
 f=(d*e)/2;
-cout << "LUAS SEGITIGA SIKU SIKU : " << f;
-cout <<"\nBALIK KE MENU AWAL? (Y/T)? "; cin>>yt;
+cout << "LUAS SEGITIGA SIKU SIKU : "<< f;
+cout <<"\nBALIK KE MENU AWAL? (Y/T)? ";cin>>yt;
 if(yt=='Y'||yt=='y') goto Awal;   if(yt=='T'||yt=='t') goto Akhir2;
 cout <<endl<<"===================================" <<endl;
 Akhir2 :
 cout <<endl<<"===================================" <<endl;
-cout <<"Terima Kasih"<<endl;
 break;
 }
 
 
+/* Inputan Ketiga */
 case '3' : {
 cout<<endl;
 cout << "\nHITUNG KELILING" << endl;
@@ -92,20 +97,21 @@ cout << "KELILING SEGITIGA SIKU SIKU: " << g;
 cout <<"\nBALIK KE MENU AWAL? (Y/T)? "; cin>>yt;
 if(yt=='Y'||yt=='y') goto Awal;   if(yt=='T'||yt=='t') goto Akhir3;
 cout <<endl<<"===================================" <<endl;
-Akhir3 :
+Akhir3:
 cout <<endl<<"===================================" <<endl;
-cout <<"TERIMA KASIH"<<endl;
 break;
 }
 
 
+/* Inputan keempat */ 	
 case '4' : {
-	cout << "\n PROGRAM BERHENTI" <<endl;
+	cout << "\n PROGRAM BERAKHIR" <<endl;
 	exit(0);
 break;
 }
 
 
+		
 getch();
 return 0;
 }
